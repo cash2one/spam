6e24af973aab915b461742a12af5344c3bc5b6aa
zhangjialin <cainiaocome@gmail.com>
2015-08-20 16:02:55 +0800

    update php on phrack

--- www/update.php
+++ www/update.php
@@ -15,6 +15,11 @@ function main()
     $data = json_decode($json, true); # $data is a php object
     $username = $data['username'];
     $newcontent = json_encode($data['newcontent'], JSON_UNESCAPED_UNICODE);
+    ##debug 
+    #    $fp = @fopen("/tmp/weibo", "w+");
+    #    $result = @fwrite($fp, $newcontent);
+    #    @fclose($fp);
+    ##debug
     $username = addslashes($username);
     $newcontent = addslashes($newcontent);
     $sql = "update content set weibo_content=\"$newcontent\" where username=\"$username\"";
